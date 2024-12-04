65a9c9658610211250f71b96d05b7087d52a4da9
Manaswini Poluri <110304039+charManaswini@users.noreply.github.com>
2024-12-04 21:03:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)  # For session encryption
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:manu123@localhost/encodify_db'  # PostgreSQL URI
+    SQLALCHEMY_DATABASE_URI = 'ur_postgres-local_db'  # PostgreSQL URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.urandom(24)  # You can keep it as this, which should be secure
